4dd6b5140842ba7bffc4f0829fec4cef78e5283c
Na11a <kuchuna@ukr.net>
2021-05-13 11:31:33 +0300

    Show All teachers and can select one

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,13 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
-
-export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
-});
+import { combineReducers, createStore } from '@reduxjs/toolkit';
+import teacherReducer from '../features/Teacher/reducer';
+import {composeWithDevTools} from "redux-devtools-extension";
+import {applyMiddleware} from 'redux'
+import thunk from 'redux-thunk'
+const composedEnhancer = composeWithDevTools(
+  applyMiddleware(thunk)
+)
+export const rootReducer = combineReducers({
+  teachers:teacherReducer
+})
+const store = createStore(rootReducer,composedEnhancer ) 
+export default store
